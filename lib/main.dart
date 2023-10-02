a0d22388e28c9885fdcd441dcd5147e487e3df8e
Jeet Rabadiya <jeetrabadiya876@gmail.com>
2023-10-02 10:36:13 +0530

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       getPages: AppRoutes.getPages,
-      initialRoute: RoutesName.homeView,
+      initialRoute: RoutesName.splashView,
     );
   }
 }
--- lib/utils/routes/app_routes.dart
+++ lib/utils/routes/app_routes.dart
@@ -2,6 +2,9 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:getx_app/utils/routes/routes_name.dart';
 import 'package:getx_app/view/Home/home_bindings.dart';
 import 'package:getx_app/view/Home/home_view.dart';
+import 'package:getx_app/view/Splash/splash_view.dart';
+
+import '../../view/Splash/splash_bindings.dart';
 
 class AppRoutes {
   static List<GetPage<dynamic>>? getPages = [
@@ -9,5 +12,9 @@ class AppRoutes {
         name: RoutesName.homeView,
         page: () => const HomeView(),
         binding: HomeBindings()),
+    GetPage(
+        name: RoutesName.splashView,
+        page: () => SplashView(),
+        binding: SplashBindings()),
   ];
 }
